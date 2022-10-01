0dadd95192261b79ba7eb3be8977564c3a7f8d61
raybarkerrr <raybarkerrr@gmail.com>
2022-09-30 20:52:39 -0500

    Time counter section
    Added the time counter section, next I'll be adding the Javascript needed to make it run.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavbarBootstrap from "./Components/Navbar.js"
 import './App.css';
 import Image from "./Components/Banner"
+import CountdownTimer from "./Components/Countdown.js";
 
 function App() {
   return (
     <>
     <NavbarBootstrap></NavbarBootstrap>
+    <CountdownTimer></CountdownTimer>
     <Image></Image>
     </>
   )
--- src/Components/Countdown.js
+++ src/Components/Countdown.js
@@ -1,2 +1,26 @@
 import "./Countdown.css"
 
+export const CountdownTimer = () => {
+    return (
+        <>
+            <div className="time-counter-container">
+                <div className="main-event">
+                    <h1>UFC 280</h1>
+                </div>
+            </div>
+            <div className="time-counter">
+                <div className="container-day">
+                    <h3 className="day">Time</h3>
+                </div>
+                <div className="container-hour">
+                    <h3 className="hour">Time</h3>
+                </div>
+                <div className="container-minute">
+                    <h3 className="minute">Time</h3>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CountdownTimer
